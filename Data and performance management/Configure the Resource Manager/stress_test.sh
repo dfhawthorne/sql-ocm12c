0054523c28c3479d6df9e682291ba4e6ade1ee31
Douglas Hawthorne (PADSTOW) <dfhawthorne@computer.org>
2019-12-12 20:45:00 +1100

    ORM stress test now runs correctly and optionally creates an AWR report

--- Data and performance management/Configure the Resource Manager/stress_test.sh
+++ Data and performance management/Configure the Resource Manager/stress_test.sh
@@ -3,6 +3,7 @@
 # Stress test all PDBs using CPU hog script
 # - Parameters:
 #     1: CDB resource plan to be tested (defaults to internal plan)
+#     2: File name for AWR report, if required.
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
@@ -15,28 +16,40 @@ export ORACLE_SID=jar
 
 # ------------------------------------------------------------------------------
 # Settings for this stress test
+# - A snapshot interval of zero (0) disables all snapshots (including manual
+#   ones)
+# - The chosen snapshot interval (120) is greater than the expected run time of
+#   the entire test.
+# - Jobs are checked every minute (60 seconds) to give a one (1) minute
+#   granualartity to the AWR reports.
 # ------------------------------------------------------------------------------
 
 new_plan_name="${1:-ORA\$INTERNAL_CDB_PLAN}"
-new_snap_interval=0
+new_snap_interval=120
+job_check_interval=60
 
 # ------------------------------------------------------------------------------
 # Get current settings
 # ------------------------------------------------------------------------------
 
-old_plan_name=$(sqlplus -S / as sysdba <<DONE
+raw_old_plan_name=$(sqlplus -S / as sysdba <<DONE
 SET HEADING OFF FEEDBACK OFF
 SELECT value FROM v\$parameter WHERE name = 'resource_manager_plan';
 EXIT
 DONE
 )
 
-old_snap_interval=$(sqlplus -S / as sysdba <<DONE
+raw_old_snap_interval=$(sqlplus -S / as sysdba <<DONE
 SET HEADING OFF FEEDBACK OFF
 SELECT 60*EXTRACT(HOUR FROM snap_interval) + EXTRACT(MINUTE FROM snap_interval) FROM DBA_HIST_WR_CONTROL;
 EXIT
 DONE
 )
+
+printf -v old_plan_name "%q" ${raw_old_plan_name}
+printf -v old_snap_interval "%d" ${raw_old_snap_interval}
+printf "Old values: plan=%s, snap_interval=%d\n" ${old_plan_name} ${old_snap_interval}
+printf "New values: plan=%s, snap_interval=%d\n" ${new_plan_name} ${new_snap_interval}
 
 # ------------------------------------------------------------------------------
 # Change settings, if needed, for this stress test.
@@ -45,7 +58,6 @@ DONE
 if [ "${new_plan_name}" != "${old_plan_name}" ]
 then
 sqlplus -S / as sysdba <<DONE
-REM Set the appropriate Resource Plan for the CDB
 ALTER SYSTEM SET resource_manager_plan="${new_plan_name}";
 EXIT
 DONE
@@ -54,7 +66,6 @@ fi
 if [ ${new_snap_interval} -ne ${old_snap_interval} ]
 then
 sqlplus -S / as sysdba <<DONE
-REM Turn off automatic AWR snapshots
 EXEC DBMS_WORKLOAD_REPOSITORY.MODIFY_SNAPSHOT_SETTINGS(interval=>${new_snap_interval});
 EXIT
 DONE
@@ -64,13 +75,16 @@ fi
 # Create an AWR snapshot
 # ------------------------------------------------------------------------------
 
-start_snap_id=$(sqlplus -S / as sysdba <<DONE
+raw_start_snap_id=$(sqlplus -S / as sysdba <<DONE
 SET HEADING OFF FEEDBACK OFF
 REM Take a manual snapshot
 SELECT DBMS_WORKLOAD_REPOSITORY.CREATE_SNAPSHOT() FROM DUAL;
 EXIT
 DONE
 )
+
+printf -v start_snap_id "%d" ${raw_start_snap_id}
+printf "Start snapshot ID=%d\n" ${start_snap_id}
 
 # ------------------------------------------------------------------------------
 # Start background jobs to hog the CPU on all selected PDBs
@@ -85,22 +99,28 @@ done
 # Wait until all jobs have completed
 # ------------------------------------------------------------------------------
 
-while [ $(jobs | wc -l) -gt 1 ]
-do sleep 300
-printf "$(date) %3d\n" $(jobs | wc -l)
+while [ $(jobs | wc -l) -gt 0 ]
+do printf "$(date) %3d jobs are running:\n" $(jobs | wc -l)
+jobs
+sleep ${job_check_interval}
 done
+printf "$(date) %3d jobs are running:\n" $(jobs | wc -l)
+jobs
 
 # ------------------------------------------------------------------------------
 # Create an AWR snapshot
 # ------------------------------------------------------------------------------
 
-end_snap_id=$(sqlplus -S / as sysdba <<DONE
+raw_end_snap_id=$(sqlplus -S / as sysdba <<DONE
 SET HEADING OFF FEEDBACK OFF
 REM Take a manual snapshot
 SELECT DBMS_WORKLOAD_REPOSITORY.CREATE_SNAPSHOT() FROM DUAL;
 EXIT
 DONE
 )
+
+printf -v end_snap_id "%d" ${raw_end_snap_id}
+printf "End snapshot ID=%d\n" ${end_snap_id}
 
 # ------------------------------------------------------------------------------
 # Restore the internal database environment after stress test
@@ -109,7 +129,6 @@ DONE
 if [ "${new_plan_name}" != "${old_plan_name}" ]
 then
 sqlplus -S / as sysdba <<DONE
-REM Set the appropriate Resource Plan for the CDB
 ALTER SYSTEM SET resource_manager_plan="${old_plan_name}";
 EXIT
 DONE
@@ -118,8 +137,30 @@ fi
 if [ ${new_snap_interval} -ne ${old_snap_interval} ]
 then
 sqlplus -S / as sysdba <<DONE
-REM Turn off automatic AWR snapshots
 EXEC DBMS_WORKLOAD_REPOSITORY.MODIFY_SNAPSHOT_SETTINGS(interval=>${old_snap_interval});
+EXIT
+DONE
+fi
+
+# ------------------------------------------------------------------------------
+# Produce an AWR Report if a file name is provided
+# ------------------------------------------------------------------------------
+
+if [ ! -z "${2}" ]
+then
+sqlplus -S / as sysdba >${2} <<DONE
+SET PAGESIZE 0 LINESIZE 1500 FEEDBACK OFF HEADING OFF VERIFY OFF
+column dbid noprint new_value dbid
+column instance_number noprint new_value instance_number
+select dbid from v\$database;
+select instance_number from v\$instance;
+select * from
+    table(DBMS_WORKLOAD_REPOSITORY.AWR_REPORT_HTML(
+        l_dbid       => &dbid.,
+        l_inst_num   => &instance_number.,
+        l_bid        => ${start_snap_id},
+        l_eid        => ${end_snap_id},
+        l_options    => 8));
 EXIT
 DONE
 fi

64cca3865f83860295f38b289e85759a7f207e5e
Douglas Hawthorne (PADSTOW) <dfhawthorne@computer.org>
2020-01-30 21:31:02 +1100

    Data Guard: display data guard status

---
+++ Data Guard/Create a physical standby database/display_dg_status.sql
@@ -0,0 +1,22 @@
+REM ====================================================================
+REM Display the status of Data Guard
+REM ====================================================================
+
+set heading off
+set feedback off
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+
+SET LINESIZE 180 PAGESIZE 100
+
+COLUMN message FORMAT A80
+
+SELECT
+    facility,
+    severity,
+    to_char(timestamp, 'YYYY-MM-DD HH24:MI:SS') AS "TIMESTAMP",
+    message
+  FROM
+    v$dataguard_status
+  ORDER BY
+    message_num
+/

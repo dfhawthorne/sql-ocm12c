640b86dbea79e1ac33b99687f853bf8320c5b00a
Douglas Hawthorne (BOTANY) <dfhawthorne@computer.org>
2020-10-21 21:50:12 +1100

    startup_standby_db: 1. Added parameters for ORACLE_HOME and ORACLE_SID 2. Includes functions from common file

--- Data Guard/Create a physical standby database/startup_standby_db.sh
+++ Data Guard/Create a physical standby database/startup_standby_db.sh
@@ -1,30 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 # ------------------------------------------------------------------------------
 # Starts up a standby database if needed
-# ------------------------------------------------------------------------------
-
-# ------------------------------------------------------------------------------
-# DESCRIPTION: Runs a SQL*Plus command against the local database instance.
 #
-#              Assumes ORACLE_SID and ORACLE_HOME have been set correctly.
-#
-# USAGE:       run_sql_cmd "SELECT * FROM DUAL;"
-#
-# RETURNS:     Error code from SQL*Plus call
-# ------------------------------------------------------------------------------
+# Parameters:
+#   1: ORACLE_SID
+#   2: ORACLE_HOME
+# -------------------------------------------------------------------------------
 
-run_sql_cmd() 
-{
-  sqlplus -L -S / as sysdba <<-DONE
-  SET HEADING OFF
-  WHENEVER SQLERROR EXIT SQL.SQLCODE
-  WHENEVER OSERROR EXIT FAILURE
-  $* 
-  EXIT
-DONE
+[ -n "$1" ] && export ORACLE_SID=$1
+[ -n "$2" ] && export ORACLE_HOME=$2
 
-  return $?
-}
+. $(dirname $0)/run_sql_cmd.sh
 
 # ------------------------------------------------------------------------------
 # Get the current status of the database instance. It should be MOUNTED for a
